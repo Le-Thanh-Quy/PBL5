b5f55e6fd9c78c5c7bbf1d0692c8f1ee6c5e5f3e
Le-Thanh-Quy <lethanhquy123321@gmail.com>
2022-04-17 22:51:02 +0700

    feture/ hello activity

--- app/src/main/java/com/quyhoangtrinhvuteam/pbl5/view/HelloActivity.java
+++ app/src/main/java/com/quyhoangtrinhvuteam/pbl5/view/HelloActivity.java
@@ -2,10 +2,15 @@ package com.quyhoangtrinhvuteam.pbl5.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Toast;
 
 import com.quyhoangtrinhvuteam.pbl5.R;
@@ -14,6 +19,7 @@ import com.quyhoangtrinhvuteam.pbl5.databinding.ActivityHelloBinding;
 public class HelloActivity extends AppCompatActivity {
 
     ActivityHelloBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +30,15 @@ public class HelloActivity extends AppCompatActivity {
         checkFocusChange();
         checkSeri();
         checkPass();
-        
+        checkNewPass();
+
     }
 
     private void checkFocusChange() {
         binding.inputKey.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                if(b){
+                if (b) {
                     focusEditText();
                 }
             }
@@ -40,7 +47,7 @@ public class HelloActivity extends AppCompatActivity {
         binding.inputKeyPass.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                if(b){
+                if (b) {
                     focusEditText();
                 }
             }
@@ -49,13 +56,12 @@ public class HelloActivity extends AppCompatActivity {
         binding.inputKeyNewPass.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
-                if(b){
+                if (b) {
                     focusEditText();
                 }
             }
         });
     }
-
 
 
     private void checkSeri() {
@@ -68,14 +74,39 @@ public class HelloActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                if(charSequence.toString().equals("taotenquy")) {
+                if (charSequence.toString().equals("quy")) {
                     binding.imgCheck.setVisibility(View.VISIBLE);
                     binding.layoutPass.setVisibility(View.VISIBLE);
+                    binding.layoutPass.setAlpha(0.0f);
+                    binding.layoutPass.animate()
+                            .translationYBy(binding.layoutPass.getHeight() * -1)
+                            .translationY(0)
+                            .alpha(1.0f)
+                            .setDuration(600)
+                            .setListener(null);
                     binding.inputKeyPass.requestFocus();
-                }else{
-                    binding.imgCheck.setVisibility(View.INVISIBLE);
-                    binding.layoutPass.setVisibility(View.INVISIBLE);
-                    binding.layoutNewPass.setVisibility(View.INVISIBLE);
+                } else {
+                    binding.imgCheck.setVisibility(View.GONE);
+                    if (binding.layoutPass.getVisibility() == View.VISIBLE) {
+                        String s = binding.inputKeyPass.getText().toString();
+                        binding.inputKeyPass.setText(" " + s + " ");
+                        binding.layoutPass.animate()
+                                .translationYBy(binding.layoutPass.getHeight())
+                                .translationY(0)
+                                .setDuration(600)
+                                .alpha(0f)
+                                .setListener(new AnimatorListenerAdapter() {
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        super.onAnimationEnd(animation);
+                                        binding.layoutPass.setVisibility(View.GONE);
+                                        binding.layoutNewPass.setVisibility(View.GONE);
+                                        binding.inputKeyNewPass.setText("");
+                                        binding.inputKeyPass.setText("");
+                                    }
+                                });
+                    }
+
                 }
             }
 
@@ -85,6 +116,7 @@ public class HelloActivity extends AppCompatActivity {
             }
         });
     }
+
     private void checkPass() {
         binding.inputKeyPass.addTextChangedListener(new TextWatcher() {
             @Override
@@ -95,13 +127,35 @@ public class HelloActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                if(charSequence.toString().equals("quy")) {
+                if (charSequence.toString().equals("quy")) {
                     binding.imgCheckPass.setVisibility(View.VISIBLE);
                     binding.layoutNewPass.setVisibility(View.VISIBLE);
+                    binding.layoutNewPass.setAlpha(0.0f);
+                    binding.layoutNewPass.animate()
+                            .translationYBy(binding.layoutNewPass.getHeight() * -1)
+                            .translationY(0)
+                            .alpha(1.0f)
+                            .setDuration(600)
+                            .setListener(null);
                     binding.inputKeyNewPass.requestFocus();
-                }else{
-                    binding.imgCheckPass.setVisibility(View.INVISIBLE);
-                    binding.layoutNewPass.setVisibility(View.INVISIBLE);
+                } else {
+                    binding.imgCheckPass.setVisibility(View.GONE);
+                    if (binding.layoutNewPass.getVisibility() == View.VISIBLE) {
+
+                        binding.layoutNewPass.animate()
+                                .translationYBy(binding.layoutNewPass.getHeight())
+                                .translationY(0)
+                                .setDuration(600)
+                                .alpha(0f)
+                                .setListener(new AnimatorListenerAdapter() {
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        super.onAnimationEnd(animation);
+                                        binding.layoutNewPass.setVisibility(View.GONE);
+                                        binding.inputKeyNewPass.setText("");
+                                    }
+                                });
+                    }
                 }
             }
 
@@ -112,7 +166,34 @@ public class HelloActivity extends AppCompatActivity {
         });
     }
 
-    private void focusEditText(){
+    private void checkNewPass() {
+        binding.inputKeyNewPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().length() > 5){
+                    binding.btnSubmit.setEnabled(true);
+                    binding.btnSubmit.setBackgroundColor(Color.parseColor("#3B8F3E"));
+                    binding.btnSubmit.setTextColor(Color.WHITE);
+                }else{
+                    binding.btnSubmit.setEnabled(false);
+                    binding.btnSubmit.setBackgroundColor(Color.parseColor("#989898"));
+                    binding.btnSubmit.setTextColor(Color.parseColor("#CDCDCD"));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void focusEditText() {
         View lastChild = binding.scrollLayout.getChildAt(binding.scrollLayout.getChildCount() - 1);
         int bottom = lastChild.getBottom() + binding.scrollLayout.getPaddingBottom();
         int sy = binding.scrollLayout.getScrollY();
